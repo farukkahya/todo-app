04630e81a1d78287e10a934a0751fcd1ca15ae97
farukkahya <farukkahya55@gmail.com>
2023-09-21 12:44:19 +0300

    TodoDTO was added and with this the todo model was created.

---
+++ src/main/java/me/farukkahya/todoapp/models/TodoDTO.java
@@ -0,0 +1,25 @@
+package me.farukkahya.todoapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Getter // alanların getter methodlarını oluşturur
+@Setter // alanların setter methodlarını oluşturur
+@AllArgsConstructor // bütün alanları içeren yapıcı methodu oluşturur
+@NoArgsConstructor // boş bir yapıcı method oluşturur
+@Document(collection = "todos") // bu nesnenin hangi koleksiyona model olacağını belirtir.
+public class TodoDTO { // todoların modeli
+    @Id
+    private String id;
+    private String todo;
+    private String description;
+    private boolean completed;
+    private Date created_at;
+    private Date updated_at;
+}
