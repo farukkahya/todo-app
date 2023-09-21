dd78e372c820899bf9b9af716c3dfdd349d91785
farukkahya <farukkahya55@gmail.com>
2023-09-21 14:48:25 +0300

    findByTodo query added

--- src/main/java/me/farukkahya/todoapp/repositories/ITodoRepository.java
+++ src/main/java/me/farukkahya/todoapp/repositories/ITodoRepository.java
@@ -2,8 +2,13 @@ package me.farukkahya.todoapp.repositories;
 
 import me.farukkahya.todoapp.models.TodoDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ITodoRepository extends MongoRepository<TodoDTO,String> {
+    @Query("{'todo': ?0}")
+    Optional<TodoDTO> findByTodo(String todo);
 }
